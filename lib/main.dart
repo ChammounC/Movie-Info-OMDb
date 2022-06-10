e0d0c020f10389d4f3ca7ab3773898084b7f8e2c
Chammoun Chakhap <43007452+ChammounC@users.noreply.github.com>
2022-06-10 22:43:50 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tradexa_movie/pages/search_page.dart';
  
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); 
 
 class MyApp extends StatelessWidget {
   @override
